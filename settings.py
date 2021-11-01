0515b63caf971a4fc8c6f27add1c105b3c2c799a
dkarapetyan <80838882+dkarapetyan@users.noreply.github.com>
2021-11-01 19:35:42 +0000

    updated completion link

--- settings.py
+++ settings.py
@@ -37,7 +37,7 @@ SESSION_CONFIGS = [
         name="Yellow",
         display_name="Yellow",
         num_demo_participants=1,
-        completionlink="https://app.prolific.co/submissions/complete?cc=881D5E05",
+        completionlink="https://app.prolific.co/submissions/complete?cc=20B7F325",
         app_sequence=['A_ExperimentIntro',
                       'B_Attention_1',
                       'C_GenInstructions_1',
